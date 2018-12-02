9c91d1faed74a6e18ae797d3146bede3c5da9740
Sarah Lucioni <sklucioni@gmail.com>
2018-12-01 23:41:07 -0500

    Sarah's commented out edit, needs to be tested

--- edit_distance.py
+++ edit_distance.py
@@ -11,8 +11,14 @@ def edit_dist(string_a, string_b):
 	# Format strings - remove punctuation and convert to lowercase
 	a_format = string_a.translate(None, string.punctuation).lower()
 	print(a_format)
+
+	# # Comment out the above and Uncomment to test Sarah's addition:
+	# # Help from https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
+	# a_format = string_a.translate(str.maketrans(dict.fromkeys(string.punctuation))).lower()
+	# print(a_format)
+
 	#sm = edit_distance.SequenceMatcher(a=a_format, b=string_b)
-	distance, matches = edit_distance.edit_distance(string_a, string_b)
+	distance, matches = edit_distance.edit_distance(a_format, string_b)
 	#distance, matches = sm.distance(), sm.matches()
 	return distance
 
