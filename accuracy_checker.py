c686b0d0b49db08276fcdc1b6459be81ee7b12e5
Sarah Lucioni <sklucioni@gmail.com>
2018-12-02 22:25:15 -0500

    fixed dictionary to reinitialize every time

--- accuracy_checker.py
+++ accuracy_checker.py
@@ -1,24 +1,24 @@
 # How to call:
-#   from accuracy_checker import accuracy_checker
+#   from accuracy_checker import Accuracy
 # Then with the text outputted from the speech-to-text function call:
-#   acc = accuracy(text)
+#   acc = Accuracy.accuracy(text)
 # and use acc as a parameter for data_to_csv
 
 class Accuracy:
-    # dictionary for gmu dataset
-    actual = {"her" : 4, "the": 3, "and" : 3, "a" : 3, "we" : 2, "things" : 2,
-              "these" : 2, "of" : 2, "for" : 2, "with" : 1, "will" : 1,
-              "wednesday" : 1, "train" : 1, "toy" : 1, "to" : 1, "three" : 1,
-              "thick" : 1, "store" : 1, "stella" : 1, "station" : 1, "spoons" : 1,
-              "snow" : 1, "snake" : 1, "snack" : 1, "small" : 1, "slabs" : 1,
-              "six" : 1, "she" : 1, "scoop" : 1, "red" : 1, "please" : 1,
-              "plastic" : 1, "peas" : 1, "need" : 1, "meet" : 1, "maybe" : 1,
-              "kids" : 1, "into" : 1, "go" : 1, "from" : 1, "frog" : 1, "fresh" : 1,
-              "five" : 1, "cheese" : 1, "can" : 1, "call" : 1, "brother" : 1,
-              "bring" : 1, "bob" : 1, "blue" : 1, "big" : 1, "bags" : 1, "at" : 1,
-              "ask" : 1, "also" : 1}
     # test is string and actual is dictionary
-    def accuracy(test, actual = actual):
+    def accuracy(test):
+        # dictionary for gmu dataset
+        actual = {"her" : 4, "the": 3, "and" : 3, "a" : 3, "we" : 2, "things" : 2,
+                  "these" : 2, "of" : 2, "for" : 2, "with" : 1, "will" : 1,
+                  "wednesday" : 1, "train" : 1, "toy" : 1, "to" : 1, "three" : 1,
+                  "thick" : 1, "store" : 1, "stella" : 1, "station" : 1, "spoons" : 1,
+                  "snow" : 1, "snake" : 1, "snack" : 1, "small" : 1, "slabs" : 1,
+                  "six" : 1, "she" : 1, "scoop" : 1, "red" : 1, "please" : 1,
+                  "plastic" : 1, "peas" : 1, "need" : 1, "meet" : 1, "maybe" : 1,
+                  "kids" : 1, "into" : 1, "go" : 1, "from" : 1, "frog" : 1, "fresh" : 1,
+                  "five" : 1, "cheese" : 1, "can" : 1, "call" : 1, "brother" : 1,
+                  "bring" : 1, "bob" : 1, "blue" : 1, "big" : 1, "bags" : 1, "at" : 1,
+                  "ask" : 1, "also" : 1}
         # lowercase test string
         test = test.lower()
 
